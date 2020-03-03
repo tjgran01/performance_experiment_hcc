ff21b40e631cec2d24349a61df80581509e8e9f3
trevor <trgr2496@colorado.edu>
2020-03-03 10:05:22 -0700

    Adding some plotting, etc.

--- scripts/data_importer.py
+++ scripts/data_importer.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import scipy
 
+import seaborn as sns
 
 def parse_ht(ht, as_cm=True):
     ht_ = ht.split("'")
@@ -10,12 +12,18 @@ def parse_ht(ht, as_cm=True):
     return (12*ft_) + inches
 
 
+def convert_to_float(x):
+
+    return float(x)
+
+
 class DataImporter(object):
     def __init__(self, data_dir="./data/", data_fname="eraser_test.csv",
                  using_sample=False, input_format="csv", demographics=False):
         self.using_sample = using_sample
         if input_format == "csv":
             self.data = self.read_in_csv_data(data_dir, data_fname)
+            self.data = self.clean_data()
         if demographics == True:
             self.demographics = pd.read_csv(f"{data_dir}{data_fname[:-4]}_demographics.csv")
             self.demographics = self.clean_demographics()
@@ -39,6 +47,12 @@ class DataImporter(object):
         return self.demographics
 
 
+    def clean_data(self):
+
+        self.data["speed"] = self.data["speed"].apply(lambda x:convert_to_float(x))
+        return self.data
+
+
     def combine_data(self):
 
         to_drop = ["group_x", "group_y"]
@@ -49,12 +63,24 @@ class DataImporter(object):
         self.combined = combined
 
 
-    def group_means(self):
+    def group_means(self, group_value="group", dep_variable="height_centimeters"):
 
-        for elm in self.combined.groupby(["group"]):
-            print(elm[1][""].describe())
+        for elm in self.combined.groupby([f"{group_value}"]):
+            print(elm[1][f"{dep_variable}"].describe())
 
 
+    def return_plot(self, x="group", y="height_centimeters", plottype="box"):
+
+        if plottype == "box":
+            ax = sns.boxplot(x=x, y=y, data=self.combined)
+        elif plottype == "reg":
+            ax = sns.regplot(x=x, y=y, data=self.combined)
+        elif plottype == "dist":
+            ax = sns.distplot(self.combined[f"{x}"].tolist())
+        return ax
+
+
+    def run_ttest(self, )
 
 
 if __name__ == "__main__":
